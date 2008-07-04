988d4e984e53c0a16b2d4f340aa1b8eaaa665a9d
scott <scott@6e1638ff-ae45-0410-89bd-df963105f760>
2008-07-04 00:07:02 +0000

    diagrams of associahedrons

--- diagrams/latex2pdf/defontify.bat
+++ diagrams/latex2pdf/defontify.bat
@@ -1,1 +1,4 @@
-bash defontify.sh
+latex defontify
+dvips -R0 defontify
+ps2pdf defontify.ps
+gs-native -r9600 -sDEVICE=pswrite -dNOCACHE -sOutputFile=nofonts.ps -q -dbatch -dNOPAUSE defontify.pdf -c quit
