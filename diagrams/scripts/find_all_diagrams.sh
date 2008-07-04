2d7b4aa868a47326416c528fb8be0041c071d276
scott <scott@6e1638ff-ae45-0410-89bd-df963105f760>
2008-07-04 00:07:02 +0000

    diagrams of associahedrons

--- diagrams/scripts/find_all_diagrams.sh
+++ diagrams/scripts/find_all_diagrams.sh
@@ -3,6 +3,7 @@ for source in `ls ../../text/*.tex`;
     do
         ./find_diagrams.sh $source
 done;
+./find_diagrams.sh ../../blob1.tex
 #./find_diagrams.sh ../../sandbox.tex
 #./find_diagrams.sh ../../todolist.tex
 ./find_diagrams.sh ../latex2pdf/defontify.tex
