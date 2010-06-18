55e04af5172ed9b452da8454e7919ba9bb66a9e2
Scott Morrison <scott@tqft.net>
2010-06-17 21:17:42 -0700

    finished rearranging diagrams directory

--- diagrams/scripts/find_diagrams.sh
+++
@@ -1,3 +0,0 @@
-grep -o '\\\mathfig{[0-9\. ]*}{[a-zA-z0-9\/_-]*}' $1 | sed -e 's/\\\mathfig{.*{\(.*\)/\1/' | sed -e 's/}//g' >> diagrams.list
-grep -o '\\\placefig{[0-9\. ]*}{[a-zA-z0-9\/_-]*}' $1 | sed -e 's/\\\placefig{.*{\(.*\)/\1/' | sed -e 's/}//g' >> diagrams.list
-grep -o '\\\rotatemathfig{ [0-9\.]*}{[0-9\.-]*}{[a-zA-z0-9\/_-]*}' $1 | sed -e 's/\\\rotatemathfig{.*{\(.*\)/\1/' | sed -e 's/}//g' >> diagrams.list
